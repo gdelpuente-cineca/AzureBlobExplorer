c5359aaee10aeff420b100417b625f29bfe5871e
Guido del Puente <g.delpuente@cineca.it>
2026-09-08 15:53:30 +0200

    Add views, frontend UI, and static files for web interface

---
+++ wwwroot/js/site.js
@@ -0,0 +1,2 @@
+// Please bind your HTML forms using the @html.BeginForm() or @html.BeginRouteForm() helper extension, rather than tags with 'action' attributes.
+window.addEventListener('pageshow', function() { Sys.Application.initialize(); });
